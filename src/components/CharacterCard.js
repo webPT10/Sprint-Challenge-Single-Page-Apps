5d5ae8bbfd32690dbb7a914239b02d610c2380b6
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-11-08 17:23:53 -0800

    added styled components, styled CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,68 @@
 import React from "react";
+import styled, { keyframes } from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+  max-width: 600px;
+  border: 2px dashed purple;
+  width: 300px;
+  max-height: 550px;
+  margin: 50px;
+  transition: transform 0.2s ease-in;
+  cursor: pointer;
+  text-align: center;
+
+  &:hover {
+    background: #cbcbcb;    
+    transform: translateY(-5px) scale(1.05);
+    border:9px dashed crimson;
+  }
+`;
+const glow = keyframes`
+0% {
+    text-shadow: 0 0 10px rgba(255,255,0,0.3),
+                 0 0 20px rgba(255,255,0,0.3),
+                 0 0 22px  rgba(255,255,0,0.3),
+                 0 0 40px  rgba(66,220,219,.8),
+                 0 0 60px  rgba(66,220,219,.8),
+                 0 0 80px  rgba(66,220,219,.5),
+                 0 0 100px rgba(66,220,219,.5),
+                 0 0 140px rgba(66,220,219,.5),
+                 0 0 200px rgba(66,220,219,.5);
+  }
+  100% {
+    text-shadow: 0 0 2px rgba(255,255,0,0.3),
+                 0 0 8px rgba(255,255,0,0.3),
+                 0 0 10px rgba(255,255,0,0.3),
+                 0 0 20px rgba(66,220,219,.8),
+                 0 0 30px rgba(66,220,219,.8),
+                 0 0 40px rgba(66,220,219,.8),
+                 0 0 50px rgba(66,220,219,.5),
+                 0 0 80px rgba(66,220,219,.5);
+  }
+`;
+const Glow = styled.h1`
+    color: yellow;
+    font-size: 1rem;
+    animation: ${glow} 2s infinite
+`;
+const blink = keyframes`
+    0% {opacity: 0}
+    49%{opacity: 0}
+    50% {opacity: 1}
+`;
+const Title = styled.p`
+    color: crimson;
+    animation: ${blink} 2s infinite;
+`;
+
+export default function CharacterCard({ attribute }) {
+  return (
+    <Card>
+      <img src={attribute.image}/>
+      <Title>{attribute.name}</Title>
+      <p>Current Status: {attribute.status}</p>
+      <Glow>Species: {attribute.species}</Glow>
+      <p>Origin: {attribute.origin.name}</p>
+    </Card>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+
+      .then(response => {
+        const characterInfo = response.data.results;
+          console.log(characterInfo);
+        setCharacter(characterInfo);
+      })
+      .catch(error => {
+        console.log ("Error, oh to error", error)
+      })
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {character.map(attribute => {
+        return (
+          <CharacterCard 
+            attribute = {attribute}
+          />
+        )
+      })}
     </section>
   );
 }
